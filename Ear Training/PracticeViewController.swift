eb24d9f5eada508c85b9b153df6025e3681b3021
Andrea Bottino <bottinoandrea93@gmail.com>
2023-03-21 21:55:41 +0100

    My second project

---
+++ Ear Training/PracticeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PracticeViewController.swift
+//  Ear Training
+//
+//  Created by Andrea Bottino on 11/03/2023.
+//
+
+import UIKit
+
+class PracticeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Ear Training/SurvivalViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SurvivalViewController.swift
+//  Ear Training
+//
+//  Created by Andrea Bottino on 13/03/2023.
+//
+
+import UIKit
+
+class SurvivalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
